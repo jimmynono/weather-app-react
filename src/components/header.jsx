e8f5e8d05a074630a88754f909dcee0d46b6b388
jamnorton <jamnorton@deloitte.com>
2018-06-19 12:52:58 -0700

    add basic button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Header() {
-  return (
-    <header className="App-header">
-      <span className="wi wi-day-sunny App-logo" alt="logo" />
-      <h1 className="App-title">Weather</h1>
-    </header>
-  )
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      user: 'James'
+    }))
+  }
+
+  render () {
+    return (
+      <header className="App-header">
+        {this.state.user && <h2>Welcome: {this.state.user}</h2>}
+        {!this.state.user && <button onClick={this.handleClick}>LOGIN</button>}
+        <span className="wi wi-day-sunny App-logo" alt="logo" />
+        <h1 className="App-title">Weather</h1>
+      </header>
+    )
+  }
 }
 
 export default Header;
